cae3db3bd8ab4bfd6ee89f8af821c2c23e565619
Marc Jauvin <marc.jauvin@gmail.com>
2020-09-23 15:53:06 -0400

    add french localized strings

---
+++ lang/fr/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'description' => 'Administration des usagers backend améliorée',
+    'labels' => [
+        'send-invite' => [
+            'credentials' => 'inclure les identifiants',
+            'reset' => 'inclure lien pour password reset',
+            'do-not-send' => 'ne pas envoyer',
+        ],
+        'purge-deleted' => 'Purger les usagers supprimés',
+        'confirm-purge-deleted' => 'Purge des usagers supprimés: êtes-vous certain?',
+    ],
+];
